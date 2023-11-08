f0c0eb2b2c1c3bc9b8f82446f61ed843b77242be
Bumbleghost <casper-github@indemailbox.nl>
2023-11-08 12:33:00 +0100

    push

--- html-template/conection.php
+++ html-template/conection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=portfolio", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=module-13eind", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
